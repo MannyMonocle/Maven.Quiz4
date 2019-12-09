e330d0b3cb8986f6553992d3d92da2ab30e01dce
MannyMonocle <eetumadu@gmail.com>
2019-12-09 18:16:33 -0500

    Array Utils

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -5,18 +5,33 @@ package rocks.zipcode.quiz4.arrays;
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        Integer middleIndex = values.length/2;
+        return values[middleIndex];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
-    }
+        String[] ans = new String[values.length - 1];
+        Integer middleIndex = values.length/2;
+        Integer w = 0;
+
+        for(int i = 0; i < values.length; i++){
+            if(i != middleIndex){
+                ans[w] = values[i];
+                w++;
+            }
+        }
+
+        return ans; }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length - 1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        String[] ans = new String[values.length - 1];
+        for(int i = 0; i < values.length - 1; i++){
+            ans[i] = values[i];
+        }
+        return ans;
     }
 }
