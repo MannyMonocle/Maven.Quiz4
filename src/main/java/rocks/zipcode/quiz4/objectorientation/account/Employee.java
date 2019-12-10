593a19bd5d7f5305af8f34d6688983b52272bce5
MannyMonocle <eetumadu@gmail.com>
2019-12-09 23:53:33 -0500

    employee

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -6,14 +6,15 @@ package rocks.zipcode.quiz4.objectorientation.account;
 public class Employee implements Worker, Transactable{
     private BankAccount account;
     private Double hours = 0.0;
-    private Double earings = 0.0;
     private Double wage;
 
     public Employee() {
         wage = 35.0;
+        account = new BankAccount();
     }
 
     public Employee(BankAccount bankAccount) {
+        wage = 35.0;
         account = bankAccount;
     }
 
@@ -26,38 +27,31 @@ public class Employee implements Worker, Transactable{
     }
 
     @Override
-    public void increaseHoursWorked(Double numberOfHours) {
-
-
-    }
+    public void increaseHoursWorked(Double numberOfHours) { hours += numberOfHours;}
 
     @Override
     public Double getHoursWorked() {
-        return null;
+        return hours;
     }
 
     @Override
     public Double getHourlyWage() {
-        return null;
+        return wage;
     }
 
     @Override
     public Double getMoneyEarned() {
-        return null;
+        return hours * wage;
     }
 
     @Override
-    public void deposit(Double amountToIncreaseBy) {
-
-    }
+    public void deposit(Double amountToIncreaseBy) { account.deposit(amountToIncreaseBy);}
 
     @Override
-    public void withdrawal(Double amountToDecreaseBy) {
-
-    }
+    public void withdrawal(Double amountToDecreaseBy) { account.withdrawal(amountToDecreaseBy);}
 
     @Override
     public Double getBalance() {
-        return null;
+        return account.getBalance();
     }
 }
