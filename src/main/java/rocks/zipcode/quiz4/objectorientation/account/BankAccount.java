82fead4f84a61e33588a7c20c69ce31b556e8343
MannyMonocle <eetumadu@gmail.com>
2019-12-09 20:32:01 -0500

    testing

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,7 +3,21 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
-    public void setBalance(Double val) {
-    }
+public class BankAccount extends Account implements Transactable{
+    private Double balance;
+
+    public void setBalance(Double val) { balance = val;}
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) throws IllegalArgumentException {
+        if(amountToIncreaseBy < 0){throw new IllegalArgumentException();}
+        balance+=amountToIncreaseBy; }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) throws IllegalArgumentException {
+        if(amountToDecreaseBy < 0 || amountToDecreaseBy > balance){throw new IllegalArgumentException();}
+        balance-=amountToDecreaseBy; }
+
+    @Override
+    public Double getBalance() { return balance; }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -4,17 +4,20 @@ package rocks.zipcode.quiz4.objectorientation.account;
  * @author leon on 30/12/2018.
  */
 public class Employee {
+    private BankAccount account;
+
     public Employee() {
     }
 
     public Employee(BankAccount bankAccount) {
+        account = bankAccount;
     }
 
     public BankAccount getBankAccount() {
-        return null;
+        return account;
     }
 
     public void setBankAccount(BankAccount bankAccount) {
-
+        account = bankAccount;
     }
 }
