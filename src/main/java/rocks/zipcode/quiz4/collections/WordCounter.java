ec998fca72173a0b4de3ae04d3224c26e646f68a
MannyMonocle <eetumadu@gmail.com>
2019-12-09 19:16:38 -0500

    calculator

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,21 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class WordCounter {
+    HashMap<String,Integer> map = new HashMap<>();
+
     public WordCounter(String... strings) {
+           for(String e : strings){
+               Integer count = (map.containsKey(e)) ? map.get(e) + 1 : 1;
+               map.put(e,count);
+           }
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        return map;
     }
 }
